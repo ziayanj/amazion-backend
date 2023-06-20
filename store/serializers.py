34f4568c048f3c490a2ad75d2ea5300e3fc278bc
Ziayan Javed <ziayanj@gmail.com>
2023-06-20 20:36:43 +0500

    Create custom signal for listening to order creation event

--- store/serializers.py
+++ store/serializers.py
@@ -2,6 +2,7 @@ from decimal import Decimal
 from django.db.models import F, Sum
 from django.db import transaction
 from rest_framework import serializers
+from .signals import order_created
 from .models import Cart, CartItem, Customer, Order, OrderItem, Product, Collection, Review
 
 
@@ -180,5 +181,7 @@ class CreateOrderSerializer(serializers.Serializer):
             OrderItem.objects.bulk_create(order_items)
 
             Cart.objects.filter(pk=cart_id).delete()
+
+            order_created.send_robust(self.__class__, order=order)
 
             return order
