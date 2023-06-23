160df6d6eb13ba172a5c960d75ddafb527724b9c
Ziayan Javed <ziayanj@gmail.com>
2023-06-23 23:52:08 +0500

    Create a custom validator for validating uploaded file size

--- store/models.py
+++ store/models.py
@@ -1,8 +1,9 @@
 from django.conf import settings
 from django.contrib import admin
-from django.core.validators import MinValueValidator
+from django.core.validators import MinValueValidator, FileExtensionValidator
 from django.db import models
 from uuid import uuid4
+from .validators import validate_file_size
 
 
 class Promotion(models.Model):
@@ -40,7 +41,8 @@ class Product(models.Model):
 
 class ProductImage(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='images')
-    image = models.ImageField(upload_to='store/images')
+    image = models.ImageField(upload_to='store/images', validators=[validate_file_size])
+    # file = models.FileField(upload_to='store/images', validators=[FileExtensionValidator(allowed_extensions=['pdf'])])
 
 
 class Customer(models.Model):
