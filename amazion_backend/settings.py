f76aad2cd36acc383ab4b1f5243c232b5b00bba3
Ziayan Javed <ziayanj@gmail.com>
2023-08-20 10:46:47 -0700

    Install and configure whitenoise to be able to serve static assets in production

--- amazion_backend/settings.py
+++ amazion_backend/settings.py
@@ -55,8 +55,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
-    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
