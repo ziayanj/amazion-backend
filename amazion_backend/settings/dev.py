81bd727a56c840bc2e71e27bf06cc0cb1bdfc223
Ziayan Javed <ziayanj@gmail.com>
2023-09-03 14:03:08 -0700

    Use Docker for containerizing the app

--- amazion_backend/settings/dev.py
+++ amazion_backend/settings/dev.py
@@ -18,17 +18,17 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'amazion',
         'USER': 'dev',
-        'HOST': 'localhost',
+        'HOST': 'mysql',
         'PORT': '5432'
     }
 }
 
-CELERY_BROKER_URL = 'redis://localhost:6379/1'
+CELERY_BROKER_URL = 'redis://redis:6379/1'
 
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
+        "LOCATION": "redis://redis:6379/2",
         "TIMEOUT": 10 * 60,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
@@ -36,7 +36,12 @@ CACHES = {
     }
 }
 
-EMAIL_HOST = 'localhost'
+EMAIL_HOST = 'smtp4dev'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 2525
+
+# Fixes debug_toolbar not working when running the app with Docker
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': lambda request: True
+}
